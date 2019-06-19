6ee05db96aca2d85c78e748fb164b496c58e7ccb
roberto951 <51999677+roberto951@users.noreply.github.com>
2019-06-19 16:43:22 -0400

    Create ProductoJs.php

---
+++ ProductoJs.php
@@ -0,0 +1,15 @@
+<?php
+class Producto
+{
+    public $costo;
+    public $id;
+
+    function __construct($costo, $id) 
+    {
+        $this->valor = $costo;
+        $this->id = $id;
+    
+    }
+}
+
+?>
